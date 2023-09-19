766cf942741e5b5983e3a92eadf524eb2f7033f3
Tiirnako <sithlord229@gmail.com>
2023-09-19 00:45:31 -0500

    Add CSS Styles
    initial styling for page created.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { app, BrowserWindow, globalShortcut, ipcMain, Tray, Menu } = require('electron');
 const path = require('path');
 
+const electron = require('electron')
+
+// Enable live reload for Electron too
+require('electron-reload')(__dirname, {
+    // Note that the path to electron may vary according to the main file
+    electron: require(`${__dirname}/node_modules/electron`)
+});
+
 let mainWindow;
 let tray = null;
 
